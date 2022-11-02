a66d9ad1489b1f119dc11ba0f1e718d708d7cd31
Danil Boldyrev <daswerq123@gmail.com>
2022-11-02 23:41:52 +0300

    Create app_colab.py

---
+++ app_colab.py
@@ -0,0 +1,28 @@
+#Install libraries
+#import subprocess
+#import sys
+
+#def install(package):
+#   subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+#install("flask")
+#install("flask-localtunnel")
+
+#app
+from flask import Flask, render_template
+f#rom flask_lt import run_with_lt
+
+app = Flask(__name__)
+ru#n_with_lt(app)
+
+@app.route('/')
+
+def home():
+   return render_template('index.html')
+if __name__ == '__main__':
+   app.run()
+
+
+
+
+
